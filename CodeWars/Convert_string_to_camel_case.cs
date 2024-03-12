a1b86d5f4835e3e0cf00231ebe44f4a344c66362
DawidKapciak <dawidkapciak@gmail.com>
2024-03-12 19:31:21 +0100

    Initial commit

---
+++ CodeWars/Convert_string_to_camel_case.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+Console.WriteLine(Kata.ToCamelCase("the-stealth-warrior"));
+Console.WriteLine(Kata.ToCamelCase("The_Stealth_Warrior"));
+Console.WriteLine(Kata.ToCamelCase("The_Stealth-Warrior"));
+
+public class Kata
+{
+    public static string ToCamelCase(string str)
+    {
+        var list = str.Split(new Char [] {'-' , '_' });
+        var res = list[0];
+
+        return list.Skip(1).Aggregate(res, (current, i) => current + string.Concat(i.First().ToString().ToUpper(), i.AsSpan(1)));
+    }
+}
