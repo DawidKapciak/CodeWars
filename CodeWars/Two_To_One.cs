eb7d9383a322ff1602213e9c45cd10140993e517
DawidKapciak <dawidkapciak@gmail.com>
2024-03-12 20:13:08 +0100

    Two To One

---
+++ CodeWars/Two_To_One.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+public class TwoToOne 
+{
+  
+    public static string Longest (string s1, string s2) 
+    {
+        return new string(s1.Concat(s2).Distinct().OrderBy(c => c).ToArray());
+
+    }
+}
